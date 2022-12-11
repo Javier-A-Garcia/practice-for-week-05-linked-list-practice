5fc5f89409e2c7322eb6785733aed0fc5325c8ea
Javier Garcia <javiergarcia7991@gmail.com>
2022-12-10 21:04:26 -0800

    Passing first phase tests

--- linked-list.js
+++ linked-list.js
@@ -8,14 +8,43 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    // Initializes head property to null
+    this.head = null;
+    // Initializes length property to 0
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    // Creates new LinkedListNode
+    const newNode = new LinkedListNode(val);
+    // Sets 'next' property of newNode to the current 'head' node
+    newNode.next = this.head;
+    // Sets 'head' of LinkedList to newNode
+    this.head = newNode;
+    // Increases length of LinkedList by 1
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+    // Creates new LinkedListNode
+    const newNode = new LinkedListNode(val);
+
+    let current = this.head;
+
+    if (current === null) {
+      this.head = newNode;
+      this.length++;
+      return;
+    }
+
+    while (current.next !== null){
+      current = current.next;
+    }
+
+    current.next = newNode;
+    this.length++;
   }
 
   // You can use this function to help debug
