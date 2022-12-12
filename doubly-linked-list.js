3d20d1d3204bad0050b191d21d0cd534b085d277
Javier Garcia <javiergarcia7991@gmail.com>
2022-12-11 18:00:55 -0800

    Passing all test on Phase 2

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -9,14 +9,48 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
   }
 
   addToHead(val) {
     // Your code here
+    // Create a new node with the given value
+    const newNode = new DoublyLinkedListNode(val);
+
+    // If list is empty, sets new node as both head and tail
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // Otherwise
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    // Increase length of LinkedList
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+    // Create a new node with the given value
+    const newNode = new DoublyLinkedListNode(val);
+
+    // If list is empty, sets new node as both head and tail
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // Otherwise
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    // Increase length of LinkedList
+    this.length++;
   }
 
   // You can use this function to help debug
